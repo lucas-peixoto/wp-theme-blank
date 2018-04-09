734dd22bcd75f08cd8d84594410b981f29166e5e
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2018-04-09 20:56:58 -0300

    Updated var name on template.php

--- templates/template.php
+++ templates/template.php
@@ -8,21 +8,21 @@ $posts = [];
 if( $loop->have_posts() ){
     while( $loop->have_posts() ) {
         $loop->the_post();
-        $post = [];
+        $p = [];
 
-        $post['thumbnail'] = get_the_post_thumbnail_url(null, 'medium', '');
-        $post['title']     = get_the_title();
-        $post['permalink'] = get_the_permalink();
-        $post['date']      = get_the_date('d-m-Y');
+        $p['thumbnail'] = get_the_post_thumbnail_url(null, 'medium', '');
+        $p['title']     = get_the_title();
+        $p['permalink'] = get_the_permalink();
+        $p['date']      = get_the_date('d-m-Y');
 
-        array_push($posts, $post);
+        array_push($posts, $p);
     } ?>
 
     <div class="row">
-    <?php foreach ($posts as $post) { ?>
-    <?php var_dump($post) ?>
+    <?php foreach ($posts as $p) { ?>
+    <?php var_dump($p) ?>
         <div class="col-12 col-sm-6 col-md-6">
-            <a href="<?= $post['permalink'] ?>"></a>
+            <a href="<?= $p['permalink'] ?>"></a>
         </div>
     <?php } ?>
     </div>
